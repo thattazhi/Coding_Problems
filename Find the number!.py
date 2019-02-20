cc782f958c847277d553f5aea7acb0589b74450f
Sajith Thattazhi <sthattaz@asu.edu>
2019-02-20 00:23:59 -0700

    New Programs

---
+++ Find the number!.py
@@ -0,0 +1,10 @@
+def findNumber(arr, k):
+
+    if k in arr:
+        return 'YES'
+
+    return 'NO'
+
+
+print(findNumber([1,2,3,4,5], 1)
+      )
---
+++ PracticeApp.py
@@ -0,0 +1,13 @@
+array = [1,2,3,4,5]
+
+int = 12
+
+string = 'asdfbhjv'
+
+print(string[::-1])
+
+for i in range(len(array) - 1, -1, -1):
+    print(array[i])
+
+words = [chr(i) for i in range(97, 123)]
+print(words)
