b85f4dc193970be5eac7f48383f026dab877a61f
Sajith Thattazhi <sajiththattazhi14@gmail.com>
2019-10-02 21:10:26 -0700

    Longest Palindromic String

---
+++ Java/LongestPalindromicString.java
@@ -0,0 +1,41 @@
+public class LongestPalindromicString 
+{
+    public String longestPalindrome(String s)
+    {
+        if(s.length() < 2) return s.length();
+
+        int max = 0, start = 0, end = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            int l1 = expand(s, i, i);
+            int l2 = expand(s, i, i + 1);
+            int l = Math.max(l1, l2);
+
+            if(l > max)
+            {
+                max = l;
+                start = i - (l - 1) / 2;
+                end = i + l / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    int expand(String s, int L, int R)
+    {
+        while(L > -1 && R < s.length() && s.charAt(L) == s.charAt(R))
+        {
+            L--;
+            R++;
+        }
+
+        return R - L - 1;
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicString l = new LongestPalindromicString();
+        System.out.println(l.longestPalindrome("babad"));
+    }
+}
