9462a410f34a1138d9c67375e992ec85f7ccb6ed
Sajith Thattazhi <sajiththattazhi14@gmail.com>
2019-10-08 18:53:22 -0700

    Search a sorted 2D Matrix

---
+++ Java/BinarySearchMatrix.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class BinarySearchMatrix {
+    static boolean binarySearchMatrix(int[][] mat, int target) {
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = { { 1, 5, 9 },
+                        { 14, 20, 21 }, 
+                        { 30, 34, 43 } };
+
+        System.out.println(binarySearchMatrix(mat, 21));
+    }
+}
