41c0c6f568b9581e58082de6276d323077ce3f80
Sajith Thattazhi <sajiththattazhi14@gmail.com>
2019-10-08 18:55:18 -0700

    Subtree of Another Tree

---
+++ Java/SubtreeOfAnotherTree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class SubtreeOfAnotherTree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null && t == null) return true;
+        
+        if(s == null || t == null) return false;
+        
+        if(isIdentical(s, t)) return true;
+        
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    boolean isIdentical(TreeNode s, TreeNode t) {
+        if(s == null && t == null) return true;
+        
+        if(s == null || t == null) return false;
+        
+        return (s.val == t.val && 
+                isIdentical(s.left, t.left) && 
+               isIdentical(s.right, t.right));
+    }
+}
